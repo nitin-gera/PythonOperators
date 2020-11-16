49b3327141168f095f80a465b40d3310cffe4f30
nitin-gera <68019550+nitin-gera@users.noreply.github.com>
2020-11-16 14:56:02 +0530

    Python Operators
    This demonstrates the use of operators in Python

---
+++ Operators.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 24 11:22:05 2018
+
+@author: nitingera
+"""
+
+operand1=10
+operand2=5.5
+operand3=10.0
+operand4=10
+
+print("sum:", operand1 + operand2)
+print("subtract:", operand1-operand2)
+print("Multiply:", operand1*operand2)
+print("Divide:", operand1/operand2)
+print("Modulus:", operand1 % operand2)
+print("Exponent:", operand1**operand2)
+print("Floor Division:", operand1//operand2)
+print(operand1==operand2)
---
+++ OperatorsExercise.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 24 11:42:16 2018
+
+@author: nitingera
+"""
+
+operand1 = 250.66
+operand2 = 1008.2
+
+print("operand1:%f operand2:%f"%(operand1, operand2))
